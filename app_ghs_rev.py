772fa7340ec34d2638fec534874a0e0083cb3f2b
dreamseeker76 <dimasyeager76@gmail.com>
2026-06-07 16:08:05 +0700

    Update app_ghs_rev.py

--- app_ghs_rev.py
+++ app_ghs_rev.py
@@ -503,8 +503,8 @@ with st.sidebar:
     st.markdown("<hr style='border-color:#1e2a3a;margin:1.2rem 0;'>", unsafe_allow_html=True)
     st.markdown("""
     <div style='font-size:.72rem;color:#3a5a7a;line-height:1.6;'>
-        <b style='color:#4a7a9a;'>Versi:</b> 1.0.0<br>
-        Released on June 7th, 2026<br>
+        <b style='color:#4a7a9a;'>Released on June 7th, 2026<br>
+        Versi:</b> 1.0.0<br>
         <b style='color:#4a7a9a;'>Basis Regulasi:</b><br>
         PP No. 22/2021 · PermenLHK P.12/2020<br>
         GHS Rev. 10 (2023) · ISO 11014
